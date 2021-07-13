56cff0f823fdbea66ee098e489068833692449dd
wswon <dnjsdntjr1@gmail.com>
2021-07-14 00:59:52 +0900

    feat(MapActivity.kt) : 실시간 위치 인디케이터 추가

--- app/src/main/java/com/tube/driver/presentation/MapActivity.kt
+++ app/src/main/java/com/tube/driver/presentation/MapActivity.kt
@@ -2,21 +2,64 @@ package com.tube.driver.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tube.driver.DLog
 import com.tube.driver.databinding.ActivityMapBinding
 import dagger.hilt.android.AndroidEntryPoint
+import net.daum.mf.map.api.MapPoint
+import net.daum.mf.map.api.MapPoint.GeoCoordinate
 import net.daum.mf.map.api.MapView
 
 @AndroidEntryPoint
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(),
+    MapView.CurrentLocationEventListener {
 
     private lateinit var binding: ActivityMapBinding
+
+    private val mapView: MapView
+        get() = binding.mapViewContainer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.mapViewContainer.addView(MapView(this))
+        mapView.setCurrentLocationEventListener(this)
+        mapView.currentLocationTrackingMode =
+            MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.currentLocationTrackingMode = MapView.CurrentLocationTrackingMode.TrackingModeOff
+        mapView.setShowCurrentLocationMarker(false)
+    }
+
+    override fun onCurrentLocationUpdate(
+        mapView: MapView?,
+        currentLocation: MapPoint?,
+        accuracyInMeters: Float
+    ) {
+        val mapPointGeo: GeoCoordinate? = currentLocation?.mapPointGeoCoord
+        DLog.d(
+            String.format(
+                "MapView onCurrentLocationUpdate (%f,%f) accuracy (%f)",
+                mapPointGeo?.latitude,
+                mapPointGeo?.longitude,
+                accuracyInMeters
+            )
+        )
+    }
+
+    override fun onCurrentLocationDeviceHeadingUpdate(p0: MapView?, p1: Float) {
+
+    }
+
+    override fun onCurrentLocationUpdateFailed(p0: MapView?) {
+
+    }
+
+    override fun onCurrentLocationUpdateCancelled(p0: MapView?) {
+
     }
 }
 

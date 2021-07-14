0aab79fb3633735235c4bdec34afdcbd2e4b8fc6
wswon <dnjsdntjr1@gmail.com>
2021-07-14 23:25:57 +0900

    feat(PlaceMapper.kt) : distance 계산해서 meter, kilometer

--- app/src/main/java/com/tube/driver/presentation/mapper/PlaceMapper.kt
+++ app/src/main/java/com/tube/driver/presentation/mapper/PlaceMapper.kt
@@ -2,6 +2,7 @@ package com.tube.driver.presentation.mapper
 
 import com.tube.driver.domain.entity.Place
 import com.tube.driver.presentation.PlaceItem
+import java.text.DecimalFormat
 
 class PlaceMapper {
 
@@ -10,7 +11,7 @@ class PlaceMapper {
             PlaceItem.Item(
                 id = id,
                 name = placeName,
-                distance = distance,
+                distance = getDisplayDistance(distance),
                 phoneNumber = phone,
                 addressName = if (roadAddressName.isNotEmpty()) roadAddressName else addressName,
                 category = category.middleCategory,
@@ -21,5 +22,16 @@ class PlaceMapper {
 
     fun transform(placeList: List<Place>): List<PlaceItem.Item> {
         return placeList.map(this::transform)
+    }
+
+    private fun getDisplayDistance(distance: String): String =
+        when {
+            distance.length > 3 -> DecimalFormat("###,###.#").format(distance.toInt() / 1000f) + KILO_METER
+            else -> distance + METER
+        }
+
+    companion object {
+        private const val KILO_METER = "km"
+        private const val METER = "m"
     }
 }
